b2d1f8c6fd74e5ae947a890357e2a7325f459af5
Dovletmammet Atamammedov <dowletmammet@gmail.com>
2019-08-13 20:42:35 -0500

    moreeee changed

--- src/test/java/Tests/ContactTest.java
+++ src/test/java/Tests/ContactTest.java
@@ -4,6 +4,6 @@ public class ContactTest {
 
     public void test1(){
         System.out.println("driver open vytrack");// it is just sudo code
-
+        System.out.println(" go check again plese");
     }
 }
